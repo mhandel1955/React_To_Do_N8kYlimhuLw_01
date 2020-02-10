adbad30fdee3dde827f302defeedcaddf5ced7f7
mhandel1955 <mhandel1955@gmail.com>
2020-02-09 20:56:25 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -11,14 +11,36 @@ class App extends React.Component {
         key: ""
       }
     };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    });
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
   }
 
   render() {
     return (
       <div className="App">
         <header>
-          <form id="to-do-form">
-            <input type="text" placehold="Enter Text" />
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placehold="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
             <button type="submit">Add</button>
           </form>
         </header>
